3e6dc2a84db3cce92560808e44aa63e2307814b7
AKHLAK ANSARI <87934721+AkhlakAnsari@users.noreply.github.com>
2021-09-12 16:13:17 +0530

    Add files via upload

---
+++ date and time in python.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+# current date and time
+now = datetime.now()
+
+# year
+print("Year:", now.year)
+#month
+print("Month:", now.month)
+#day
+print("Day =", now.day)
+#hour
+print("Hour:", now.hour)
+#minute
+print("Minute:", now.minute)
+#second
+print("Second:", now.second)
+
+#thanks akhlakansari94@gmail.com
